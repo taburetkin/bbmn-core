e0aa5a275cea93268d8f3c7811b488f4aede40d1
dimaver <dima.taburetkin@gmail.com>
2018-09-28 16:55:03 +0300

    instanbul ignore in marionette.js

--- src/marionette.js
+++ src/marionette.js
@@ -1,6 +1,7 @@
 export { Region } from 'backbone.marionette';
 import { Object as oldObject, MnObject } from 'backbone.marionette';
 
+/* istanbul ignore next */
 const newObject = MnObject || oldObject;
 
 export {
